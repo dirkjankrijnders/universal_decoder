c0fcdc4e3dc8d9cb2738aad5a6f5e82347deb245
Dirkjan Krijnders <dirkjankrijnders@incas3.eu>
2016-06-14 17:54:13 +0200

    Generate the python files form the qt-designer files

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
+
+# Borrowed from: https://github.com/glue-viz/glue/blob/master/setup.py
+# Copyright (c) 2013, Glue developers
+
+import os
+import subprocess
+import sys
+
+from glob import glob
+from setuptools import Command
+
+from buildqt import BuildQt
 
 setup(name='Decoder Configurator',
       version='1.0',
@@ -8,15 +20,16 @@ setup(name='Decoder Configurator',
       author='Dirkjan Krijnders',
       author_email='dirkjan@krijnders.net',
       url='',
-      packages=['decconf', 'decconf.ui', 'decconf.protocols', 'decconf.interfaces', 'decconf.datamodel', 'decoders', 'entry_points'],
-	  install_requires=['pyside', 'appdirs', 'yapsy', 'pyserial'],
+      #packages=['decconf', 'decconf.ui', 'decconf.protocols', 'decconf.interfaces', 'decconf.datamodel', 'decoders', 'entry_points'],
+      packages = find_packages(),
+      cmdclass={'build_qt': BuildQt},
+      install_requires=['appdirs', 'yapsy', 'pyserial'],
       entry_points={
             'gui_scripts': [
                   'decconf=decconf:main',
                   ],
-            },
             'console_scripts': [
                   'bulkLNCVAccess=bulkLNCVaccess:main',
                   ],
             },
-     )
+)
