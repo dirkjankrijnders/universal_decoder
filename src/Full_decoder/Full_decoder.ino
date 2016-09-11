43036d885a4a84bc32f051d9e733975833471e4a
Dirkjan Krijnders <dirkjan@krijnders.net>
2016-09-11 12:38:14 +0200

    Move debug code to config.h in libdec

--- src/Full_decoder/Full_decoder.ino
+++ src/Full_decoder/Full_decoder.ino
@@ -1,20 +1,6 @@
 #include <Servo.h>
 
-/*
-If serial output is desired uncommment the #undef line. For production, i.e.
-without computer connected this line should be commented out. 
-*/
-#define DEBUG_OUTPUT
-#undef DEBUG_OUTPUT
-
-#ifdef DEBUG_OUTPUT
-#define USE_SERIAL
-#define DEBUG(x) Serial.print(x)
-#define DEBUGLN(x) Serial.println(x)
-#else
-#define DEBUG(x)
-#define DEBUGLN(x)
-#endif
+#include "config.h"
 
 /* We're a loconet decoder! */
 #include <LocoNet.h>
