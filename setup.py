a07c3609114752747bcc2bfda1842ac69447b0a1
Dirkjan Krijnders <dirkjan@ieee.org>
2017-04-04 05:35:02 +0000

    Made setup.py adhere to PEP-0008

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
+""" Setuptools configuration for decconf"""
+
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages
 
 # Borrowed from: https://github.com/glue-viz/glue/blob/master/setup.py
 # Copyright (c) 2013, Glue developers
-
-import os
-import subprocess
-import sys
-
-from glob import glob
-from setuptools import Command
 
 from decconf.buildqt import BuildQt
 
@@ -20,16 +15,16 @@ setup(name='decconf',
       author='Dirkjan Krijnders',
       author_email='dirkjan@krijnders.net',
       url='',
-      #packages=['decconf', 'decconf.ui', 'decconf.protocols', 'decconf.interfaces', 'decconf.datamodel', 'decoders', 'entry_points'],
-      packages = find_packages(),
+      packages=find_packages(),
       cmdclass={'build_qt': BuildQt},
       install_requires=['appdirs', 'yapsy', 'pyserial', 'Qt.py'],
       entry_points={
-            'gui_scripts': [
-                  'decconf=decconf.entry_points.decoderconf:main',
-                  ],
-            'console_scripts': [
-                  'bulkLNCVAccess=decconf.entry_points.bulkLNCVaccess:main',
-                  ],
-            },
-)
+          'gui_scripts': [
+              'decconf=decconf.entry_points.decoderconf:main',
+          ],
+          'console_scripts': [
+              'bulkLNCVAccess=decconf.entry_points.bulkLNCVaccess:main',
+          ],
+          },
+     )
+
