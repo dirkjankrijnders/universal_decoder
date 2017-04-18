e9a9ed494ef2f14df547e8f2dcd0fe95425f4a00
Dirkjan Krijnders <dirkjan@krijnders.net>
2017-04-18 12:14:23 +0000

    Passes pylint

--- decconf/entry_points/decoderconf.py
+++ decconf/entry_points/decoderconf.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
+""" Entry point for Qt QUI """
 
 import sys
 
-from Qt import QtCore, QtGui, QtWidgets
+from Qt import QtWidgets
 
 
 from decconf.ui.controlmainwindow import ControlMainWindow
 
 def main():
-	"""docstring for main"""
-	app = QtWidgets.QApplication(sys.argv)
-	app.setApplicationName("Decoder Configurator")
-	mySW = ControlMainWindow()
-	mySW.show()
-	sys.exit(app.exec_())
-	
+    """ Nothing but a Qt application starter """
+    app = QtWidgets.QApplication(sys.argv)
+    app.setApplicationName("Decoder Configurator")
+    my_gui = ControlMainWindow()
+    my_gui.show()
+    sys.exit(app.exec_())
+
 if __name__ == '__main__':
-	main()
+    main()
