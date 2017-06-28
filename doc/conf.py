dfb3d944430b1c6bf7fbf3e6ebc12a523c4f4414
Dirkjan Krijnders <dirkjan@krijnders.net>
2017-06-28 16:53:44 +0200

    Sphinx tweaks

--- doc/conf.py
+++ doc/conf.py
@@ -89,7 +89,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -163,4 +163,4 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'https://docs.python.org/3/': None}
