335689c86abad0902630bacd08c54ca462b31ef7
Dirkjan Krijnders <dirkjan@krijnders.net>
2017-07-05 07:05:26 +0200

    Last style fixes

--- decconf/interfaces/locobuffer.py
+++ decconf/interfaces/locobuffer.py
@@ -16,13 +16,13 @@ class LocoBuffer(object):
         self.oq = output_queue
         self.mq = Queue()  # collections.deque()
         self.running = True
-        self.lastsend = b''
+        self.last_send = b''
 
         self.reader = threading.Thread(None, self.read)
         self.reader.daemon = True
         self.reader.start()
 
-        self.writer = threading.Thread(None, self.writet)
+        self.writer = threading.Thread(None, self.write_thread)
         self.writer.daemon = True
         self.writer.start()
 
@@ -37,10 +37,10 @@ class LocoBuffer(object):
         while self.running:
             buf += self.serial.read(1)
             if recieve_loconet_bytes(buf):
-                if buf == self.lastsend:
+                if buf == self.last_send:
                     pass
                 else:
-                    self.logger.debug("Recv LN Msg: 0x {}".format(" ".join("{:02x}".format(b) for b in buf)))
+                    self.logger.debug("Received LN Msg: 0x {}".format(" ".join("{:02x}".format(b) for b in buf)))
                     # if not self.mq.empty() and self.mq[0].match(buf): # We have a expected reply, pop the message
                     if self.expectMessage is not None and self.expectMessage.match(buf):
                         self.expectCondition.acquire()
@@ -61,12 +61,12 @@ class LocoBuffer(object):
         self.logger.debug("Push to send Queue: 0x {}".format(" ".join("{:02x}".format(b) for b in buf)))
         self.mq.put(LNMessage(buf))
 
-    def writet(self):
+    def write_thread(self):
         while self.running:
             msg = self.mq.get()
             self.logger.debug("Sending to serial: 0x {}".format(" ".join("{:02x}".format(b) for b in msg.msg)))
             self.serial.write(msg.msg)
-            self.lastsend = msg.msg
+            self.last_send = msg.msg
             if msg.expectReply:
                 self.expectCondition.acquire()
                 self.expectMessage = msg
