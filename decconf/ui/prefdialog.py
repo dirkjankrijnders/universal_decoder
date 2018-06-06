01e457487303f52fcf89d97b1743a1bdae8f3127
Dirkjan Krijnders <dirkjan@krijnders.net>
2018-06-06 21:04:10 +0200

    PEP8 update

--- decconf/ui/prefdialog.py
+++ decconf/ui/prefdialog.py
@@ -1,61 +1,57 @@
-#from Qt import QtCore, QtGui
-#from Qt import QtWidgets
 import serial.tools.list_ports
 
-#from decconf.ui.preferences import Ui_Dialog 
-from Qt import QtCore, QtGui, QtWidgets, QtCompat
+from Qt import QtCore, QtCompat
+
 
 class PreferenceDialog(object):
-	"""docstring for PreferenceDialog"""
-	def __init__(self, config):
-		super(PreferenceDialog, self).__init__()
-		self.config = config
-		self.ui = QtCompat.loadUi("decconf/ui/preferences.ui")
-		#self.ui.setupUi(self);
-		self.applyConfig();
-		
-		self.ui.deviceCombo.addItem("{}".format("None"), userData = None);
-		for ii, port in enumerate(serial.tools.list_ports.comports()):
-			self.ui.deviceCombo.addItem("{}".format(port[0]), userData = port);
-		self.ui.deviceCombo.addItem("{}".format("Dummy"), userData = 'dummy');
-		
-		portIndex = self.ui.deviceCombo.findText(self.config.get('general', 'device'))
-		print("Found port index ", portIndex)
-		if portIndex >=0:
-			self.ui.deviceCombo.setCurrentIndex(portIndex);
-		self.ui.detectCheckBox.stateChanged.connect(self.detectChanged)
-		self.ui.connectCheckBox.stateChanged.connect(self.connectChanged)
-		self.ui.deviceCombo.currentIndexChanged.connect(self.deviceChanged)
-		# self.ui.show()
-		#QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), Dialog.accept)
-		
-	def applyConfig(self):
-		if self.config.getboolean("general", "autodetect"):
-			self.ui.detectCheckBox.setCheckState(QtCore.Qt.Checked)
-		else:
-			self.ui.detectCheckBox.setCheckState(QtCore.Qt.Unchecked)
-		
-		if self.config.getboolean("general", "autoconnect"):
-			self.ui.connectCheckBox.setCheckState(QtCore.Qt.Checked)
-		else:
-			self.ui.connectCheckBox.setCheckState(QtCore.Qt.Unchecked)
-	
-	def detectChanged(self):
-		if self.ui.detectCheckBox.checkState() == QtCore.Qt.Checked:
-			self.config.set("general", "autodetect", 'True');
-		else:
-			self.config.set("general", "autodetect", 'False');
+    """docstring for PreferenceDialog"""
 
-	def connectChanged(self):
-		if self.ui.connectCheckBox.checkState() == QtCore.Qt.Checked:
-			self.config.set("general", "autoconnect", 'True');
-		else:
-			self.config.set("general", "autoconnect", 'False');
+    def __init__(self, config):
+        super(PreferenceDialog, self).__init__()
+        self.config = config
+        self.ui = QtCompat.loadUi("decconf/ui/preferences.ui")
+        self.apply_config()
 
-	def deviceChanged(self):
-		port = self.ui.deviceCombo.currentText();
-		
-		if port == "None":
-			self.config.set("general", "device", "")
-		else:
-			self.config.set("general", "device", port)
+        self.ui.deviceCombo.addItem("{}".format("None"), userData=None)
+        for ii, port in enumerate(serial.tools.list_ports.comports()):
+            self.ui.deviceCombo.addItem("{}".format(port[0]), userData=port)
+        self.ui.deviceCombo.addItem("{}".format("Dummy"), userData='dummy')
+
+        port_index = self.ui.deviceCombo.findText(self.config.get('general', 'device'))
+        print("Found port index ", port_index)
+        if port_index >= 0:
+            self.ui.deviceCombo.setCurrentIndex(port_index)
+        self.ui.detectCheckBox.stateChanged.connect(self.detect_changed)
+        self.ui.connectCheckBox.stateChanged.connect(self.connect_changed)
+        self.ui.deviceCombo.currentIndexChanged.connect(self.device_changed)
+
+    def apply_config(self):
+        if self.config.getboolean("general", "autodetect"):
+            self.ui.detectCheckBox.setCheckState(QtCore.Qt.Checked)
+        else:
+            self.ui.detectCheckBox.setCheckState(QtCore.Qt.Unchecked)
+
+        if self.config.getboolean("general", "autoconnect"):
+            self.ui.connectCheckBox.setCheckState(QtCore.Qt.Checked)
+        else:
+            self.ui.connectCheckBox.setCheckState(QtCore.Qt.Unchecked)
+
+    def detect_changed(self):
+        if self.ui.detectCheckBox.checkState() == QtCore.Qt.Checked:
+            self.config.set("general", "autodetect", 'True')
+        else:
+            self.config.set("general", "autodetect", 'False')
+
+    def connect_changed(self):
+        if self.ui.connectCheckBox.checkState() == QtCore.Qt.Checked:
+            self.config.set("general", "autoconnect", 'True')
+        else:
+            self.config.set("general", "autoconnect", 'False')
+
+    def device_changed(self):
+        port = self.ui.deviceCombo.currentText()
+
+        if port == "None":
+            self.config.set("general", "device", "")
+        else:
+            self.config.set("general", "device", port)
