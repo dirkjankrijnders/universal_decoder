ae596a822b9cc3d04e3fb6242184bb5521138796
Dirkjan Krijnders <dirkjan@krijnders.net>
2022-01-02 12:05:46 +0100

    Update debug environment

--- .gdbinit
+++ .gdbinit
@@ -1,3 +1,3 @@
-file .pioenvs/sparkfun_promicro16/firmware.hex
-target remote localhost:4242
+# file .pio/build/sparkfun_promicro16/firmware.elf
+# target remote localhost:4242
 
--- src/config.h
+++ src/config.h
@@ -1,8 +1,21 @@
-
+// #define DEBUG_OUTPUT 1
 #ifdef DEBUG_OUTPUT
+#define USE_SERIAL 1
+#warning src debug
+#define DEBUG(x) Serial.print(x);
+#define DEBUG2(x, y) Serial.print(x, y);
+#define DEBUGLN(x) Serial.println(x);
+#else
+#define DEBUG(x)
+#define DEBUG2(x, y)
+#define DEBUGLN(x)
+#endif
+
+
+/*#ifdef DEBUG_OUTPUT
 #ifndef USE_SERIAL 
 #define USE_SERIAL 1
 #endif
 #endif
-
+*/
 #define PIN_SERVO 0
